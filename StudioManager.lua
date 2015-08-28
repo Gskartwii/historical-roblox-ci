8f19740d9f6ba38fa59e8154a8721bd33e77caec
GSKW <agoldstar101@gmail.com>
2015-08-28 16:24:22 +0300

    The drive letter changed again <_<

--- StudioManager.lua
+++ StudioManager.lua
@@ -6,7 +6,7 @@ function StudioManager.StartClient()
 end
 
 function StudioManager.StartServer()
-	io.popen((os.getenv("LOCALAPPDATA") .. "\\Roblox\\Versions\\version-536a65c8f5284f3f\\RobloxStudioBeta.exe -testMode -fileLocation F:/Valkyrie_CI/RunFile.rbxl -script \"%s\""):format(io.open("Studio_Server.lua", "r"):read("*all"):gsub("\"", "\\\"")):gsub("&", "^&"), "r"):close();
+	io.popen((os.getenv("LOCALAPPDATA") .. "\\Roblox\\Versions\\version-536a65c8f5284f3f\\RobloxStudioBeta.exe -testMode -fileLocation D:/Valkyrie_CI/RunFile.rbxl -script \"%s\""):format(io.open("Studio_Server.lua", "r"):read("*all"):gsub("\"", "\\\"")):gsub("&", "^&"), "r"):close();
 end
 
 function StudioManager.GetOutput()
