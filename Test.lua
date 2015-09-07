4995b1dabe903375037f81a5ddc129311edfe29c
GSKW <agoldstar101@gmail.com>
2015-09-07 16:05:34 +0000

    Add shebang, prepare for Lapis

--- Test.lua
+++ Test.lua
@@ -1,15 +1,11 @@
+#!/usr/bin/env luajit
+local BranchName = ...;
 local DirScanner = dofile "DirScanner.lua";
 local TableFormatter = dofile "TableFormatter.lua";
 local BuildRBXM	= dofile "BuildRBXM.lua";
-local Result = DirScanner.ScanDirectory "i:/Valkyrie Git";
+local Result = DirScanner.ScanDirectory "branches/" .. BranchName;
 local StudioManager = dofile "StudioManager.lua";
 
-local Handle, Err = io.open(os.getenv("LOCALAPPDATA") .. "\\Roblox\\Versions\\version-536a65c8f5284f3f\\content\\Valkyrie.rbxm", "wb");
+local Handle, Err = io.open("builds/" .. BranchName .. ".rbxm");
 Handle:write(BuildRBXM.BuildFromTable(TableFormatter.FormatTable(Result)));
 Handle:close(); -- Allow Studio to access the file
-
---StudioManager.StartClient();
---StudioManager.StartServer();
---local Output = StudioManager.GetOutput();
---print("Client output:", Output.ClientOutput);
---print("Server output:", Output.ServerOutput);
