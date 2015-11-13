346c738a25557ae8caba211fdb01f6964559c3ff
GSKW <agoldstar101@gmail.com>
2015-11-13 17:13:01 +0200

    Accord to the new Roblox API (again)

--- HTTPFunctions.lua
+++ HTTPFunctions.lua
@@ -1,17 +1,15 @@
 local SSLWrapper = require "ssl";
 local Sockets = require "socket";
 
-local function BuildRequest(URL, FormData, ExtraHeaders)
+local function BuildRequest(URL, FormData, ExtraHeaders, Host)
 	local Request = "POST " .. URL .. " HTTP/1.1\n";
-	Request = Request .. "Host: www.roblox.com\n";
+	Request = Request .. "Host: " .. (Host or "www.roblox.com") .. "\n";
 	Request = Request .. "Accept: */*\n";
 	Request = Request .. "Connection: close\n"; -- keep-alive will never yield EOF so you can't tell if the server has finished
 	Request = Request .. "Content-Length: " .. FormData:len() .. "\n";
 	Request = Request .. "User-Agent: Roblox/WinINet\n"; -- Some pages require this I think
 	Request = Request .. ExtraHeaders .. "\n";
 	Request = Request .. FormData;
-
-    print(Request);
 
 	return Request;
 end
@@ -42,6 +40,15 @@ local function HTTPGet(URL, ExtraHead)
     Socket:send(BuildRequest(URL, "", ExtraHead):gsub("POST", "GET")); -- lol
     local Response = Socket:receive("*a");
     Socket:close();
+    return Response;
+end
+
+local function DataRequest(URL, FormData, ExtraHeaders)
+	local Socket = Sockets.tcp();
+	Socket:connect("data.roblox.com", 80);
+	Socket:send(BuildRequest(URL, FormData, ExtraHeaders, "data.roblox.com"));
+	local Response = Socket:receive("*a");
+    print(Response);
     return Response;
 end
 
@@ -77,4 +84,4 @@ local function GetCSRF()
     return CSRFToken;
 end
 
-return {BuildRequest, HTTPRequestSSL, HTTPRequest, StripHeaders, Login, GetCSRF};
+return {BuildRequest, HTTPRequestSSL, HTTPRequest, StripHeaders, Login, DataRequest};
--- Uploader.lua
+++ Uploader.lua
@@ -1,8 +1,8 @@
-local BuildRequest, HTTPRequestSSL, HTTPRequest, StripHeaders, Login, GetCSRF = unpack(require("HTTPFunctions"));
+local BuildRequest, HTTPRequestSSL, HTTPRequest, StripHeaders, Login, DataRequest = unpack(require("HTTPFunctions"));
 
 local Upload;
 Upload = function(Data, ID, Name, SessionCookie, Force)
-	local Result = HTTPRequest("/Data/Upload.ashx?assetid=" .. ID .. "&type=Model&name=" .. Name  .. "&description=Valkyrie%20CI%20upload&genreTypeId=1&ispublic=True&allowComments=True",
+	local Result = DataRequest("/Data/Upload.ashx?assetid=" .. ID .. "&type=Model&name=" .. Name  .. "&description=Valkyrie%20CI%20upload&genreTypeId=1&ispublic=True&allowComments=True",
 		Data, "Cookie: " .. SessionCookie .. "\nContent-Type: text/xml\n");
 	if Result:match("/RobloxDefaultErrorPage") then
 		if Force then
