3bbde8664666284ea38f96962c62a0f4d529f4d4
GSKW <agoldstar101@gmail.com>
2015-12-04 18:27:46 +0200

    SSL time!

--- GitInfo.lua
+++ GitInfo.lua
@@ -7,7 +7,7 @@ return function(BranchID, HeadCommitID, HeadCommitText, Author)
     local BaseDir = "branches/" .. BranchID .. "/MainModule.mod.lua/GitMeta/";
     lfs.mkdir(BaseDir);
     
-    io.open(BaseDir .. "BranchID.StringValue", "w"):write(StringValueTemplate:format(BranchID));
+    assert(io.open(BaseDir .. "BranchID.StringValue", "w")):write(StringValueTemplate:format(BranchID));
     io.open(BaseDir .. "HeadCommitID.StringValue", "w"):write(StringValueTemplate:format(HeadCommitID));
     io.open(BaseDir .. "HeadCommitText.StringValue", "w"):write(StringValueTemplate:format(HeadCommitText));
     io.open(BaseDir .. "Author.StringValue", "w"):write(StringValueTemplate:format(Author));
