40e6cd3393b33a6ac329483138302db78cec9799
GSKW <agoldstar101@gmail.com>
2015-12-28 11:38:34 +0200

    Fix CI links for the final time

--- app.lua
+++ app.lua
@@ -77,7 +77,7 @@ local function ReactToWebhook(RepoID, BranchID, BranchName, CommitID, CommitMess
     ApplyGitInformation(BranchID, CommitID, CommitMessage, CommitPusher);
 
     if not Success then
-        GitHubStatus(CommitID, RepoID, "error", "The build failed due to an error in the CI", "https://rbxvalkyrie.dy.fi:444/build_log/" .. CommitID);
+        GitHubStatus(CommitID, RepoID, "error", "The build failed due to an error in the CI", "https://ci.crescentcode.net/build_log/" .. CommitID);
         local File = io.open("build_logs/" .. CommitID .. ".log", "w");
         File:write(Error);
         File:close();
@@ -86,13 +86,13 @@ local function ReactToWebhook(RepoID, BranchID, BranchName, CommitID, CommitMess
         local File = io.open("build_logs/" .. CommitID .. ".log", "w");
         File:write(BuildResult);
         File:close();
-        GitHubStatus(CommitID, RepoID, "failure", "The build failed due to an error in the repository", "https://rbxvalkyrie.dy.fi:444/build_log/" .. CommitID);
+        GitHubStatus(CommitID, RepoID, "failure", "The build failed due to an error in the repository", "https://ci.crescentcode.net/build_log/" .. CommitID);
     else
         local File = io.open("build_logs/" .. CommitID .. ".log", "w");
         File:write(BuildResult);
         File:close();
         local ModelID = AttemptUpload(BranchID);
-        GitHubStatus(CommitID, RepoID, "success", "The build succeeded", "https://rbxvalkyrie.dy.fi:444/build_log/" .. CommitID);
+        GitHubStatus(CommitID, RepoID, "success", "The build succeeded", "https://ci.crescentcode.net/build_log/" .. CommitID);
     end
 
     return {layout = false; render = "empty"; content_type = "text/plain"; BuildResult};
