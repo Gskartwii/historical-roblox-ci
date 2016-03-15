dc5b6d6eeb91f297e9db5f4fe720986b0946ff93
CrescentCode <gskartwii@gmail.com>
2016-03-15 13:31:49 -0400

    [#6] Better model names
    Still need to add proper descriptions though

--- Uploader.lua
+++ Uploader.lua
@@ -4,6 +4,7 @@ local Upload;
 Upload = function(Data, ID, Name, SessionCookie, Force)
 	local Result = DataRequest("/Data/Upload.ashx?assetid=" .. ID .. "&type=Model&name=" .. Name  .. "&description=Valkyrie%20CI%20upload&genreTypeId=1&ispublic=True&allowComments=True",
 		Data, "Cookie: " .. SessionCookie .. "\nContent-Type: text/xml\n");
+        print(Result);
 	if Result:match("/RobloxDefaultErrorPage") then
 		if Force then
 			error("ROBLOX LOGIN FAILED! Please contact gskw. Remember to include the time this happened at.");
@@ -14,6 +15,12 @@ Upload = function(Data, ID, Name, SessionCookie, Force)
 end
 
 return function(ID, BranchName)
-	local ModelID = Upload(io.open("builds/" .. BranchName .. ".rbxm"):read("*all"), ID, BranchName, io.open("session.cookie", "r"):read("*all"));
+    local ModelName = "";
+    if BranchName:find "CrescentCode/ValkyrieFramework/" then
+        ModelName = BranchName:gsub("CrescentCode/ValkyrieFramework/", "");
+    else
+        ModelName = BranchName:gsub("/ValkyrieFramework/", "/");
+    end
+	local ModelID = Upload(io.open("builds/" .. BranchName .. ".rbxm"):read("*all"), ID, ModelName, io.open("session.cookie", "r"):read("*all"));
 	return ModelID;
 end;
