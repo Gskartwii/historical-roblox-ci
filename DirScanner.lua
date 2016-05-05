825d0de5f996d2b9a1e508a163b88b80eb13bd56
Skye <exp.lunate@gmail.com>
2016-05-05 19:37:54 +0100

    Update DirScanner.lua
    Remove the now unneeded *.moon check.

--- DirScanner.lua
+++ DirScanner.lua
@@ -89,7 +89,7 @@ RecursiveScanDir = function(InstanceTree, Name, UnderscoreTable)
                 if UnderscoreTable and GetName(File) == "_" then
                     if CurrentFileExtension:find("%.lua") then
                         UnderscoreTable.Properties.Source = {0x1, FileContent};
-                    elseif not CurrentFileExtension:find("%.moon") then
+                    else
                         local Properties, NewLog;
                         if FileContent ~= "" then
                             Properties, NewLog	= SafeDecode(Name .. "/" .. File, FileContent);
